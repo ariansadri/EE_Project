08fc0cba270a0aab756152e92eb036439c0fa8ef
Arian Sadri <ariansadri17@gmail.com>
2019-11-25 19:17:10 -0600

    push

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home_page():
-    return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
